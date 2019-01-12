629f429a11b96865dc25c034a0b116fde99f2a88
28JayG <jaygandhi.32@outlook.com>
2019-01-12 22:45:55 +0530

    * Gives a Screen to the HomePage * Creates restaurantScreen and otherScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hidden_drawer_menu/models/restaurant.dart';
+import 'package:hidden_drawer_menu/sceerns/other_screen.dart';
+import 'package:hidden_drawer_menu/sceerns/restaurant_screen.dart';
+import 'package:hidden_drawer_menu/models/screen.dart';
 import 'package:hidden_drawer_menu/widgets/restaurant_card.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,62 +18,91 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//TODO:(1.1) Let's take care of the appbar and scaffold background
-
-//TODO:(1.2.1) Make a List[of RestaurantCards]
-
-
-
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+// TODO:(2.1) Give your HomePage A Screen.
+
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+
+//  final Screen screen = otherScreen;
+
+//  _MyHomePageState({this.screen});
+
+//  @override
+//  Widget build(BuildContext context) {
 //    return Container(
 //      decoration: BoxDecoration(
-//        image: DecorationImage(
-//          image: AssetImage('assets/wood_bk.jpg'),
-//          fit: BoxFit.cover,
-//        ),
+//        image: screen.backgroundImage,
 //      ),
 //      child: Scaffold(
-//        backgroundColor: Colors.transparent,
-//        appBar: AppBar(
-//          title: Text(
-//            'RESTAURANTS',
-//            style: TextStyle(
-//              fontFamily: 'bebas-neue',
-//              fontSize: 25.0,
-//            ),
-//          ),
-//          leading: IconButton(
-//            icon: Icon(Icons.menu),
-//            onPressed: () {
-//              print('Menu');
-//            },
-//          ),
 //          backgroundColor: Colors.transparent,
-//          elevation: 0.0,
-//        ),
-////        body: ListView(
-////          children: <Widget>[
-////            RestaurantCard(),
-//////          RestaurantCard(
-//////            restaurant: restaurants[0],
-//////          ),
-//////          RestaurantCard(
-//////            restaurant: restaurants[1],
-//////          ),
-//////          RestaurantCard(
-//////            restaurant: restaurants[2],
-//////          ),
-////          ],
-////        ),
+//          appBar: AppBar(
+//            title: Text(
+//              screen.title,
+//              style: TextStyle(
+//                fontFamily: 'bebas-neue',
+//                fontSize: 25.0,
+//              ),
+//            ),
+//            leading: IconButton(
+//              icon: Icon(Icons.menu),
+//              onPressed: () {
+//                print('Menu');
+//              },
+//            ),
+//            backgroundColor: Colors.transparent,
+//            elevation: 0.0,
+//          ),
+//          body: screen.contentBuilder(context),
 //      ),
 //    );
+//  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/wood_bk.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(
+            'RESTAURANTS',
+            style: TextStyle(
+              fontFamily: 'bebas-neue',
+              fontSize: 25.0,
+            ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              print('Menu');
+            },
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: ListView(
+            children:[
+                RestaurantCard(
+                restaurant: restaurants[0],
+                ),
+                RestaurantCard(
+                restaurant: restaurants[1],
+                ),
+                RestaurantCard(
+                restaurant: restaurants[2],
+                ),
+            ],
+        ),
+      ),
+    );
   }
 }
--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,46 +1,45 @@
-//import 'package:flutter/material.dart';
-//// TODO:(1.2.3) Create Restaurant Model
-//class Restaurant {
-//  final String imageURI;
-//  final String name;
-//  final String address;
-//  final int likes;
-//  final IconData icon;
-//  final Color iconBackgroundColor;
-//
-//  const Restaurant({
-//    this.imageURI = '',
-//    this.name = '',
-//    this.address = '',
-//    this.likes = 0,
-//    this.icon,
-//    this.iconBackgroundColor,
-//  });
-//}
-//
-//const restaurants = [
-//  Restaurant(
-//    imageURI: 'assets/eggs_in_skillet.jpg',
-//    address: 'Main Road, Bistupur',
-//    likes: 45,
-//    icon: Icons.fastfood,
-//    iconBackgroundColor: Colors.red,
-//    name: 'Le Bon',
-//  ),
-//  Restaurant(
-//    imageURI: 'assets/steak_on_cooktop.jpg',
-//    address: 'C.H.Area, Sonari',
-//    likes: 32,
-//    icon: Icons.restaurant,
-//    iconBackgroundColor: Colors.blue,
-//    name: 'The Naan Sense',
-//  ),
-//  Restaurant(
-//    imageURI: 'assets/spoons_of_spices.jpg',
-//    address: 'Market Area, Sonari',
-//    likes: 25,
-//    icon: Icons.fastfood,
-//    iconBackgroundColor: Colors.green,
-//    name: 'Sukh Sagar',
-//  ),
-//];
+import 'package:flutter/material.dart';
+class Restaurant {
+  final String imageURI;
+  final String name;
+  final String address;
+  final int likes;
+  final IconData icon;
+  final Color iconBackgroundColor;
+
+  const Restaurant({
+    this.imageURI = '',
+    this.name = '',
+    this.address = '',
+    this.likes = 0,
+    this.icon,
+    this.iconBackgroundColor,
+  });
+}
+
+const restaurants = [
+  Restaurant(
+    imageURI: 'assets/eggs_in_skillet.jpg',
+    address: 'Main Road, Bistupur',
+    likes: 45,
+    icon: Icons.fastfood,
+    iconBackgroundColor: Colors.red,
+    name: 'Le Bon',
+  ),
+  Restaurant(
+    imageURI: 'assets/steak_on_cooktop.jpg',
+    address: 'C.H.Area, Sonari',
+    likes: 32,
+    icon: Icons.restaurant,
+    iconBackgroundColor: Colors.blue,
+    name: 'The Naan Sense',
+  ),
+  Restaurant(
+    imageURI: 'assets/spoons_of_spices.jpg',
+    address: 'Market Area, Sakchi',
+    likes: 25,
+    icon: Icons.fastfood,
+    iconBackgroundColor: Colors.green,
+    name: 'Sukh Sagar',
+  ),
+];
